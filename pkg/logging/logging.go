6ccd48d3311882a7fad9610187d9a356b000af20
Fred Mayer <f.mayer@pr-lg.ru>
2023-08-28 16:57:50 +0300

    Add logrus logging

---
+++ pkg/logging/logging.go
@@ -0,0 +1,51 @@
+package logging
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+
+func Log() *logrus.Logger {
+	return logger
+}
+
+func Init(level string) {
+	l := logrus.New()
+
+	l.SetReportCaller(true)
+
+	l.SetFormatter(&logrus.TextFormatter{
+		CallerPrettyfier: func(f *runtime.Frame) (function string, file string) {
+			filename := path.Base(f.File)
+			return fmt.Sprintf("%s()", f.Function), fmt.Sprintf("%s:%d", filename, f.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	})
+
+	err := os.MkdirAll("logs", 0644)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+
+	// logFile, err := os.OpenFile("logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0640)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	lvl, err := logrus.ParseLevel(level)
+	if err != nil {
+		panic(err)
+	}
+
+	l.SetLevel(lvl)
+
+	//l.SetOutput(io.Discard)
+	logger = l
+}
